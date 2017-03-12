17510f41778572ed040d3a278bd7c1cc62f20e1f
bintiM <marc.bintinger@yahoo.com>
2017-03-12 09:20:26 +0100

    initial

---
+++ QuickSort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unsorted = { 23, 12, 1, 56, 76, 3, 21, 110 };
+
+            // Print the unsorted array
+            for (int i = 0; i < unsorted.Length; i++)
+                Console.Write(unsorted[i] + " ");
+
+            Console.WriteLine();
+
+            // Sort the array
+            Quicksort(unsorted, 0, unsorted.Length - 1);
+
+            // Print the sorted array
+            for (int i = 0; i < unsorted.Length; i++)
+                Console.Write(unsorted[i] + " ");
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+            int tmp = 0;
+
+            while (i <= j)
+            {
+                while (elements[i] < pivot)
+                    i++;
+
+                while (elements[j]  > pivot)
+                    j--;
+
+                if (i <= j)
+                {
+                    // Swap
+                    tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+                Quicksort(elements, left, j);
+
+            if (i < right)
+                Quicksort(elements, i, right);
+        }
+
+    }
+}
